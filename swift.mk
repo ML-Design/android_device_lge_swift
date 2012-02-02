aa054845114d57cf70681d61b65db86851aa2902
IceCreamSwift <miroslav_mm@rambler.ru>
2012-02-02 17:19:21 +0200

    + LedNotifications

--- swift.mk
+++ swift.mk
@@ -63,6 +63,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
         librs_jni \
+		LedNotifications \
 #        LiveWallpapers \
 #        LiveWallpapersPicker \
 #        VisualizationWallpapers \
